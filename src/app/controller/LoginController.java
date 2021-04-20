f9ee675be4d6c3c33e805d2bd59808f809a3be47
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-20 16:48:19 +0100

    nn

---
+++ src/app/controller/LoginController.java
@@ -0,0 +1,20 @@
+package app.controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class LoginController {
+
+    @FXML
+    private TextField userInserido;
+    @FXML
+    private TextField passInserida;
+    @FXML
+    private Button butEntrar;
+
+
+
+
+
+}
