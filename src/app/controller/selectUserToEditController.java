2a5a23426ee48566500be439eceeed80874562d5
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-22 18:25:35 +0100

    Interfaces 18h25 22/04

---
+++ src/app/controller/selectUserToEditController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class selectUserToEditController {
+    public Pane funcionariosSelectEditPane;
+    public TextField usernameEditFunc;
+    public Button btnConfirmEditFunc;
+    public Button btnCancelEditFunc;
+    public CheckBox checkEditFunc;
+}
