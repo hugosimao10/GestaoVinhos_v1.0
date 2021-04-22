049c6f816662797165e48383403e402ae74690e8
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-22 18:56:18 +0100

    Interfaces 18h56 22/04

---
+++ src/app/controller/plantacoesController.java
@@ -0,0 +1,34 @@
+package app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class plantacoesController {
+    public Pane plantacoesPane;
+    public TableView tablePlantacoes;
+    public Button btnAddPlantacao;
+    public Button btnEditPlantacao;
+    public Button btnConcluirPlantacao;
+    public TableView tableCastas;
+    public TableColumn tabelCastas;
+    public Button btnAddCasta;
+    public Button btnRemoveCasta;
+
+    public void btnAddPlantClic(ActionEvent actionEvent) {
+    }
+
+    public void btnConcluirPlantClic(ActionEvent actionEvent) {
+    }
+
+    public void btnEditPlantClic(ActionEvent actionEvent) {
+    }
+
+    public void btnAddCastaClic(ActionEvent actionEvent) {
+    }
+
+    public void btnRemoveCastaClic(ActionEvent actionEvent) {
+    }
+}
