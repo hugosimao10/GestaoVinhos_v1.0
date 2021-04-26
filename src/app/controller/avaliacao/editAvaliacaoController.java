20e0c7b35d2b47077a3e0974e5cb3aabca0e7e44
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-26 14:55:14 +0100

    Tudo Pronto!! 14h55 26/04

---
+++ src/app/controller/avaliacao/editAvaliacaoController.java
@@ -0,0 +1,21 @@
+package app.controller.avaliacao;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class editAvaliacaoController {
+    public Pane editAvaliacaoPane;
+    public TextField numControlo;
+    public TextField qtdProduzida;
+    public Button btnConfirmEditAvaliacao;
+    public Button btnCancelEditAvaliacao;
+    public TextField qualidadeVinho;
+
+    public void btnEditAvaliacaoAddClic(ActionEvent actionEvent) {
+    }
+
+    public void btnEditAvaliacaoCancelarClic(ActionEvent actionEvent) {
+    }
+}
---
+++ src/app/controller/embalamento/embalamentoController.java
@@ -0,0 +1,24 @@
+package app.controller.embalamento;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class embalamentoController {
+    public Pane embalamentosPane;
+    public TableView table;
+    public TableColumn colNum;
+    public TableColumn colQtdCaixas;
+    public TableColumn colTipoVinho;
+    public TableColumn colData;
+    public Button btnAddEmbalamento;
+    public Button btnEditEmbalamento;
+
+    public void btnAddEmbalamentoClic(ActionEvent actionEvent) {
+    }
+
+    public void btnEditEmbalamentoClic(ActionEvent actionEvent) {
+    }
+}
