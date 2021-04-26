b5703e9a2b0bd2ceffbb2c235826b5b1631e8c63
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-26 11:45:08 +0100

    Update Vindimas 11:44 26/04

---
+++ src/app/controller/addVindimaController.java
@@ -0,0 +1,22 @@
+package app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class addVindimaController {
+    public Pane vindimasPane;
+    public TextField usernameFunc;
+    public TextField numPlant;
+    public Button btnConfirmAddVindima;
+    public Button btnCancelAddFunc;
+    public DatePicker dtIniVindima;
+
+    public void btnAddVindimaClic(ActionEvent actionEvent) {
+    }
+
+    public void btnAddVindimaCancelarClic(ActionEvent actionEvent) {
+    }
+}
---
+++ src/app/controller/concluirVindimaController.java
@@ -0,0 +1,21 @@
+package app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class concluirVindimaController {
+    public Pane concluirVindimaPane;
+    public TextField numVindima;
+    public Button btnConfirmConcluirVindima;
+    public Button btnCancelConcluirVindima;
+    public CheckBox checkConcluirVindima;
+
+    public void btnConcluirVindimaClic(ActionEvent actionEvent) {
+    }
+
+    public void btnConcluirVindimaCancelarClic(ActionEvent actionEvent) {
+    }
+}
